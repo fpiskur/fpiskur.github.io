d6d74d13a6cee01cd3310b7e0a3c23ffa3580d15
fpiskur <filip.piskur@gmail.com>
2019-12-02 18:08:39 +0100

    Updates

---
+++ precache-manifest.a05ec96d8c57095378219d49bbc6a84c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fcd8f5679b4524f941f56bc59fb864",
+    "url": "/artfolio/index.html"
+  },
+  {
+    "revision": "2d3a0d04849f2ee78d75",
+    "url": "/artfolio/static/css/main.7d3e1a14.chunk.css"
+  },
+  {
+    "revision": "6cf095f4c6e454715c45",
+    "url": "/artfolio/static/js/2.22f1469a.chunk.js"
+  },
+  {
+    "revision": "2d3a0d04849f2ee78d75",
+    "url": "/artfolio/static/js/main.8488cfac.chunk.js"
+  },
+  {
+    "revision": "b99a41a11ba73a0ef489",
+    "url": "/artfolio/static/js/runtime~main.7e57c4b6.js"
+  },
+  {
+    "revision": "80028ad20bcfe821c3818c01cbf5758e",
+    "url": "/artfolio/static/media/fa-user.80028ad2.png"
+  },
+  {
+    "revision": "dd643e38ac4feb7db22cfe6a79d31577",
+    "url": "/artfolio/static/media/special-image.dd643e38.png"
+  },
+  {
+    "revision": "d25880661bf9faac6e3f5b86fe0cb477",
+    "url": "/artfolio/static/media/tv-header-image.d2588066.png"
+  }
+]);
